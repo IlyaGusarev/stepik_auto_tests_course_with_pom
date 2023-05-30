fb1062a938ac42d8964d614d0b3ee1e9e0b45f1a
IlyaGusarev <gusarev353@gmail.com>
2023-05-30 11:23:42 +0300

    Add __init__.py, conftest.py and test_main_page.py

--- conftest.py
+++ conftest.py
@@ -9,9 +9,9 @@ from selenium.webdriver.firefox.options import Options as OptionsFirefox
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_name',
-        action = 'store',
-        default = 'chrome',
-        help = 'Choose browser'
+        action='store',
+        default='chrome',
+        help='Choose browser'
     )
     parser.addoption(
         '--language',
@@ -28,7 +28,9 @@ def browser(request):
     browser = None
     if browser_name == 'chrome':
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': language}
+        )
         browser = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
         install_dir = "/snap/firefox/current/usr/lib/firefox"
