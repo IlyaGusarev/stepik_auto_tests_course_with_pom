398b8145e44b8d22486a325a75c15c18d7acc961
IlyaGusarev <gusarev353@gmail.com>
2023-06-02 11:17:54 +0300

    Remove time.sleep() method

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,6 @@
 # pytest -v -s --tb=line --reruns 2 --language=en test_product_page.py
 
 from .pages.product_page import ProductPage
-import time
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -10,6 +9,5 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.open()
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
-    # time.sleep(99999)
     product_page.product_name_should_be_in_success_message()
     product_page.basket_price_should_match_with_product_price()
